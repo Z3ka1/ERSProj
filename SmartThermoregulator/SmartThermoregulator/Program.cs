3c906c4b03737c08b58b6ed13f412296f352694f
Sanja111 <sanjarikic0@gmail.com>
2022-12-18 17:50:03 +0100

    Doradjena ogranicenja za UI

--- SmartThermoregulator/SmartThermoregulator/Program.cs
+++ SmartThermoregulator/SmartThermoregulator/Program.cs
@@ -15,19 +15,60 @@ namespace SmartThermoregulator
 
 
 
-            //TODO Izmeniti ucitavanje vremena, napraviti zastitu unosa
+            //TODO Izmeniti ucitavanje vremena, napraviti zastitu unosa 
+            while (true)
+            {
                 Console.WriteLine("Unesite od koliko sati pocinje dnevni rezim!");
                 com = Console.ReadLine();
-                od = Int32.Parse(com);
+                if(Int32.TryParse(com, out od) != true)
+                {
+                    continue;
+                }
+                if (od <= 23 && od >= 0)
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
                 Console.WriteLine("Unesite do koliko sati traje dnevni rezim!");
                 com = Console.ReadLine();
-                doo = Int32.Parse(com);
+                if(Int32.TryParse(com, out doo) != true)
+                {
+                    continue;
+                }
+                if (doo <=23 && doo >= 0)
+                {
+                    break;
+                }
+            }
+            while (true)
+            {
                 Console.WriteLine("Unesite temperaturu dnevnog rezima!");
                 com = Console.ReadLine();
-                temperaturaDnevnog = Int32.Parse(com);
+                if(Int32.TryParse(com, out temperaturaDnevnog) != true)
+                {
+                    continue;
+                }
+                if (temperaturaDnevnog >= 0 && temperaturaDnevnog <= 35)
+                {
+                    break;
+                }
+            }
+            while (true)
+            {
                 Console.WriteLine("Unesite temperaturu nocnog rezima!");
                 com = Console.ReadLine();
-                temperaturaNocnog = Int32.Parse(com);
+                if (Int32.TryParse(com, out temperaturaNocnog) != true)
+                {
+                    continue;
+                }
+                if (temperaturaNocnog >= 0 && temperaturaNocnog <= 35)
+                {
+                    break;
+                }
+            }
                 Console.WriteLine("Izvrsavanje...");
 
 
