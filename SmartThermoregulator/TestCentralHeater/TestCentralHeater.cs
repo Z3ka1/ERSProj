0a99a306de74fa8bd3ac50e92e3571a96f430103
Marko Ankić <120253601+Z3ka1@users.noreply.github.com>
2023-01-09 01:51:33 +0100

    Pokusaj testiranja i dokumentacija
    Pokusaj testiranja klasa
    +
    Zavrsena dokumentacija

--- SmartThermoregulator/TestCentralHeater/TestCentralHeater.cs
+++ SmartThermoregulator/TestCentralHeater/TestCentralHeater.cs
@@ -1,11 +1,69 @@
 ﻿using System;
 using Common;
 using CentralHeater;
+using NUnit.Framework;
+using System.Threading;
 
 namespace TestCentralHeater
 {
     public class TestCentralHeater
     {
+        private CentralHeater.CentralHeater _centralHeater;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _centralHeater = new CentralHeater.CentralHeater();
+        }
+
+        [Test]
+        public void TestTurnOn()
+        {
+            bool expected = true;
+
+            _centralHeater.TurnOn();
+            bool actual = _centralHeater.IsOn();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestTurnOff()
+        {
+            bool expected = false;
+
+            _centralHeater.TurnOn();
+            _centralHeater.TurnOff();
+            bool actual = _centralHeater.IsOn();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsOn_ShouldReturnCorrectValueForIsOn()
+        {
+            _centralHeater.isOn = true;
+
+            Assert.IsTrue(_centralHeater.IsOn());
+
+            _centralHeater.isOn = false;
+            Assert.IsFalse(_centralHeater.IsOn());
+        }
+
+
+        [Test]
+        public void TestGetRunTime()
+        {
+            TimeSpan expected = TimeSpan.FromSeconds(30);
+
+            _centralHeater.TurnOn();
+            Thread.Sleep(30000); // Sleep 30 seconds
+            _centralHeater.TurnOff();
+            TimeSpan actual = _centralHeater.GetRunTime();
+
+            Assert.That(actual, Is.EqualTo(expected).Within(TimeSpan.FromMilliseconds(100)));
+
+        }
     }
 }
 
